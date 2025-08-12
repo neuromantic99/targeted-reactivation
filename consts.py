99acf68d3fd1aec0257df8b62e31d027c1fdb21d
neuromantic99 <jrowlandm@gmail.com>
2025-08-12 14:30:21 +0100

    assign sleep state to ripples and save result

--- consts.py
+++ consts.py
@@ -2,3 +2,8 @@ from pathlib import Path
 
 
 LOCAL_SSD = Path("/Volumes/hard_drive/targeted_reactivation/")
+
+DETECTION_METHOD = "median"  # options 'median' or 'sd'
+
+RIPPLE_BAND = [120, 250]
+SUPRA_RIPPLE_BAND = [250, 500]
