55ca9ecf356b3a5c7e6ba289923654e4e9567baa
neuromantic99 <jrowlandm@gmail.com>
2025-08-12 18:49:27 +0100

    add spindle detection reorganise

---
+++ detect_spindles.py
@@ -0,0 +1,185 @@
+import json
+from pathlib import Path
+from typing import List
+from matplotlib import pyplot as plt
+import numpy as np
+from ripples.utils import (
+    compute_power,
+    bandpass_filter,
+    threshold_detect,
+    compute_envelope,
+    get_event_frequency,
+)
+
+from models import CandidateSpindle, SpindleCache
+from utils import get_lfp_index_sleep_state
+
+
+HERE = Path(__file__).parent
+
+
+def detect_spindles(
+    mouse: str,
+    imec: str,
+    rsc_low: int,
+    rsc_high: int,
+    data_folder: Path,
+    sampling_rate_lfp: float,
+    lfp: np.ndarray,
+) -> None:
+    # The mean across all channels was subtracted (common average reference) and the
+    # mean across all times was subtracted from each channel to normalise
+    lfp = lfp[rsc_low:rsc_high, :]
+    common_average_time = np.mean(lfp, axis=0)
+    lfp = np.subtract(lfp, common_average_time)
+    common_average_channel = np.mean(lfp, axis=1)
+    lfp = np.subtract(lfp, common_average_channel[:, np.newaxis])
+
+    # After bandpass filtering the data from one cortical region at a time in the sigma band (10-16Hz)
+    # using a zero-phase, second order Butterworth filter, the channel with the highest power in the sigma
+    # band was selected for spindle detection.
+    sigma_filtered = bandpass_filter(lfp, 10, 16, sampling_rate_lfp, order=2)
+    max_power_channel = np.argmax(compute_power(sigma_filtered))
+
+    # The filtered signal from this channel then underwent a Hilbert transform, and the absolute value of this was
+    # taken as a proxy for instantaneous amplitude, or power. Candidate spindle events were identified where the
+    # signal exceeded +2 standard deviations above the mean, and the start and end of these events was defined as
+    # when the signal decayed below +0.2 standard deviations above the mean.
+    envelope = compute_envelope(sigma_filtered)
+
+    candidate_spindles = detect_candidate_spindles(
+        envelope[max_power_channel, :], sampling_rate_lfp
+    )
+    control_band = bandpass_filter(lfp, 20, 30, sampling_rate_lfp, order=2)
+
+    print(f"Number of candidate spindles: {len(candidate_spindles)}")
+
+    # Candidate events were excluded if the instantaneous amplitude in a control frequency band of 20-30Hz exceeded
+    # +4.5 standard deviations above the mean, in order to discount broadband power increases.
+
+    control_band_envelope = compute_envelope(control_band)
+
+    candidate_spindles = control_band_filter(
+        candidate_spindles, control_band_envelope[max_power_channel, :]
+    )
+
+    print(
+        f"Number of candidate spindles after control band filter: {len(candidate_spindles)}"
+    )
+
+    # Finally, the putative spindle events were required to be between 0.5 – 2.5 seconds in duration.
+    #  In order to avoid duplicated detections where the signal oscillated over the detection threshold within a single event,
+    #  if multiple events had the same start and end times, the version with the highest amplitude peak was kept, and the duplicates discarded.
+    spindles = length_check(candidate_spindles, sampling_rate_lfp)
+
+    print(f"Number of candidate spindles after length check: {len(candidate_spindles)}")
+
+    lfp_state_idx = get_lfp_index_sleep_state(
+        data_folder=data_folder,
+        # This mouse was done with buffering rather than streaming.
+        # Need to remove ripples past the 30 minute mark below
+        n_samples=lfp.shape[1] if mouse != "00053" else 30 * 60 * sampling_rate_lfp,
+        sampling_rate_lfp=sampling_rate_lfp,
+    )
+
+    spindle_state = []
+
+    for spindle in spindles:
+        for state, idxs in lfp_state_idx.items():
+            if spindle.peak_idx in idxs:
+                spindle_state.append(state)
+                break
+
+    assert len(spindle_state) == len(spindles)
+
+    cache_result = SpindleCache(
+        spindles=spindles,
+        length_recording=(
+            lfp.shape[1] / sampling_rate_lfp if mouse != "00053" else 30 * 60
+        ),
+        state=spindle_state,
+        state_lengths={state: len(list(idxs)) for state, idxs in lfp_state_idx.items()},
+    )
+
+    with open(
+        HERE / "results" / "spindles" / f"{mouse}_{imec}.json",
+        "w",
+    ) as f:
+        json.dump(cache_result.model_dump(), f)
+
+
+def length_check(
+    candidate_spindles: List[CandidateSpindle], sampling_rate: float
+) -> List[CandidateSpindle]:
+    filtered_spindles = []
+    for spindle in candidate_spindles:
+        duration = (spindle.offset - spindle.onset) / sampling_rate
+        print(duration)
+        if 0.5 <= duration <= 2.5:
+            filtered_spindles.append(spindle)
+
+    return filtered_spindles
+
+
+def control_band_filter(
+    candidate_spindles: List[CandidateSpindle], control_band: np.ndarray
+) -> List[CandidateSpindle]:
+    filtered_spindles = []
+    threshold = np.mean(control_band) + 4.5 * np.std(control_band)
+    for spindle in candidate_spindles:
+        control_band_segment = control_band[spindle.onset : spindle.offset]
+        if np.max(control_band_segment) < threshold:
+            filtered_spindles.append(spindle)
+    return filtered_spindles
+
+
+def detect_candidate_spindles(
+    sigma_filtered: np.ndarray,
+    sampling_rate: float,
+) -> List[CandidateSpindle]:
+
+    candidate_spindles: List[CandidateSpindle] = []
+    upper_threshold = np.mean(sigma_filtered) + 2 * np.std(sigma_filtered)
+    lower_threshold = np.mean(sigma_filtered) + 0.2 * np.std(sigma_filtered)
+
+    in_event = False
+    upper_exceeded = False
+    peak_amp = -np.inf
+
+    for idx, value in enumerate(sigma_filtered):
+
+        if value > lower_threshold and not in_event:
+            start_event = idx
+            in_event = True
+
+        if in_event and value > peak_amp:
+            peak_amp = value
+            peak_idx = idx
+
+        # If you bounce on the lower threshold
+        if value < lower_threshold and in_event and not upper_exceeded:
+            in_event = False
+
+        if value > upper_threshold:
+            upper_exceeded = True
+
+        if value < lower_threshold and in_event and upper_exceeded:
+            in_event = False
+            upper_exceeded = False
+            # plt.axvline(start_event / 10, color="b", linestyle="--")
+            # plt.axvline(idx / 10, color="b", linestyle="--")
+
+            candidate_spindles.append(
+                CandidateSpindle(
+                    onset=start_event,
+                    offset=idx,
+                    peak_amplitude=peak_amp,
+                    peak_idx=peak_idx,
+                    frequency=get_event_frequency(
+                        sigma_filtered[start_event:idx], sampling_rate
+                    ),
+                )
+            )
+            peak_amp = -np.inf
+
+    return candidate_spindles
--- lfp_signatures.py
+++ lfp_signatures.py
@@ -3,7 +3,9 @@ from pathlib import Path, PureWindowsPath
 from typing import Dict, List, Tuple
 import numpy as np
 import pandas as pd
-from ripples.utils import compute_power, bandpass_filter, threshold_detect
+from ripples.utils import (
+    threshold_detect,
+)
 import seaborn as sns
 import traceback
 
@@ -11,15 +13,10 @@ import traceback
 from npyx import extract_rawChunk, read_metadata
 
 from ripples.utils_npyx import load_sync_npyx
-from ripples.ripple_detection import (
-    get_candidate_ripples,
-    remove_duplicate_ripples,
-    get_quality_metrics,
-)
-from consts import DETECTION_METHOD, LOCAL_SSD, RIPPLE_BAND
+from consts import LOCAL_SSD
+from detect_spindles import detect_spindles
 from gsheets_importer import gsheet2df
 from models import RipplesCache
-from plotting import plot_lfp_profile, plot_lfp_spectrogram
 
 from data_import import Session
 from main import HERE, get_aligners
@@ -31,92 +28,9 @@ from utils import get_data_paths
 HERE = Path(__file__).parent
 
 
-def process_sleep_spreadsheet(data_path: Path) -> Tuple[np.ndarray, np.ndarray]:
-    num_to_state = {0.5: "nrem", 0: "deep nrem", 1: "rem", 2: "awake", 4: "movement"}
-
-    mouse = data_path.parts[-1]
-
-    spreadsheet_path = Path(
-        "/Volumes/MarcBusche/Alex/Reactivations/Sleep Scoring/results"
-    )
-
-    spreadsheets = list(spreadsheet_path.glob(f"*.xlsx"))
-    mouse_sheets = [
-        file_name
-        for file_name in spreadsheets
-        if mouse in str(file_name).lower()
-        and "tones" not in str(file_name).lower()
-        and file_name.name[:2] != "~$"  # Exclude temporary files
-    ]
-    assert (
-        len(mouse_sheets) == 1
-    ), f"Expected one sleep scoring spreadsheet for mouse {mouse}, found {len(mouse_sheets)}."
-    spreadsheet = mouse_sheets[0]
-
-    data = pd.read_excel(spreadsheet, sheet_name="Sheet1")
-
-    mins = data["Minutes"].to_numpy()
-    seconds = data["Seconds"].to_numpy()
-    score = data["Score"].to_numpy()
-
-    # Data missing at the end of the spreadsheet, had a look at the video. Mouse is moving a lot so assigned to awake
-    if "10681" in str(data_path):
-        first_nan = np.where(np.isnan(score))[0][0]
-        assert first_nan > 29 * 60
-        assert np.all(np.isnan(score[first_nan:]))
-        score[first_nan:] = 2  # awake
-
-    total_seconds = mins * 60 + seconds
-
-    # Mistake in all spreadsheets where the minute is set to 5 when it should be 6
-    total_seconds[368] = 368
-    assert np.all(np.diff(total_seconds) == 1)
-
-    return total_seconds, np.array([num_to_state[state] for state in score])
-
-
-def get_detection_channels_CA1(
-    ca1: np.ndarray, ca1_low: int, ca1_high: int, sampling_rate_lfp: float
-) -> np.ndarray:
-
-    # # Find CA1 channel with highest Ripple power and +/- two channels to detect ripples, then do CAR
-    swr_power = compute_power(
-        bandpass_filter(ca1, RIPPLE_BAND[0], RIPPLE_BAND[1], sampling_rate_lfp, order=4)
-    )
-
-    ca1_max_power_channel = np.nanargmax(swr_power)
-
-    if ca1_max_power_channel - 2 < 0:
-        ca1_max_power_channel += 1
-        print("Detection channels moved to stay in CA1")
-
-    all_ca1_channels = np.arange(ca1_low, ca1_high)
-    assert ca1_max_power_channel + 3 < len(all_ca1_channels)
-    assert ca1_max_power_channel - 2 >= 0
-
-    # CA1_channels are the channels in CA1 used for ripple detection
-    detection_channels_ca1 = all_ca1_channels[
-        ca1_max_power_channel - 2 : ca1_max_power_channel + 3
-    ]
-
-    # If the reference channel is part of the selected channels for ripple analysis replace with neighbouring channel with the higher ripple power
-    if 191 in detection_channels_ca1:
-        detection_channels_ca1.remove(191)
-        lower_channel = all_ca1_channels[ca1_max_power_channel - 3]
-        if (ca1_max_power_channel + 3) > (len(all_ca1_channels) - 1):
-            swr_pow_higher_channel = 0
-        else:
-            higher_channel = all_ca1_channels[ca1_max_power_channel + 3]
-            swr_pow_higher_channel = swr_power[higher_channel]
-        if swr_power[lower_channel] > swr_pow_higher_channel:
-            detection_channels_ca1.append(lower_channel)
-        else:
-            detection_channels_ca1.append(higher_channel)
-
-    return detection_channels_ca1
-
-
-def get_ripple_rate(lfp_path: Path, region_channels: Tuple[int, int, int, int]) -> None:
+def get_lfp_signatures(
+    lfp_path: Path, region_channels: Tuple[int, int, int, int]
+) -> None:
     mouse = lfp_path.parent.parent.name
     imec = f"imec_{str(lfp_path).split('imec')[1]}"
 
@@ -149,20 +63,18 @@ def get_ripple_rate(lfp_path: Path, region_channels: Tuple[int, int, int, int]) 
     assert abs(aligners[1].units_B - meta["lowpass"]["sampling_rate"]) < 1
     sampling_rate_lfp = meta["lowpass"]["sampling_rate"]
 
-    # start_rest2 =
-
     resting_lfp_path = LOCAL_SSD / f"resting_lfps/{mouse}_{imec}.npy"
     if (resting_lfp_path).exists():
         print("loading existing resting LFP")
         lfp = np.load(resting_lfp_path)
     else:
-        # start_rest = aligners[1].first_matched_time_A / sampling_rate_lfp
-        # end_rest = aligners[1].last_matched_time_A / sampling_rate_lfp
         frame_triggers = np.load(frame_trigger_times[1])
         start_rest, end_rest = (
             aligners[1].B_to_A(np.array([frame_triggers[0], frame_triggers[-1]]))
             / sampling_rate_lfp
         )
+        assert start_rest < end_rest, "Start time must be before end time"
+        assert 10 * 60 > end_rest - start_rest > 40 * 60
         lfp = extract_rawChunk(
             lfp_path,
             [start_rest, end_rest],  # now taking the recording length as a float
@@ -195,188 +107,8 @@ def get_ripple_rate(lfp_path: Path, region_channels: Tuple[int, int, int, int]) 
     #     imec=imec,
     # )
 
-    detect_ripples(mouse, imec, ca1_low, ca1_high, data_folder, sampling_rate_lfp, lfp)
-
-
-def detect_ripples(
-    mouse: str,
-    imec: str,
-    ca1_low: int,
-    ca1_high: int,
-    data_folder: Path,
-    sampling_rate_lfp: float,
-    lfp: np.ndarray,
-) -> None:
-    ca1_lfp = lfp[ca1_low:ca1_high, :]
-    detection_channels_ca1 = get_detection_channels_CA1(
-        ca1_lfp, ca1_low, ca1_high, sampling_rate_lfp
-    )
-
-    assert len(detection_channels_ca1) == 5, "Expected 5 detection channels in CA1"
-    assert min(detection_channels_ca1) >= ca1_low
-    assert max(detection_channels_ca1) < ca1_high
-
-    ca1_lfp_detection_channels_only = lfp[detection_channels_ca1, :]
-    common_average = np.nanmedian(ca1_lfp, axis=0)
-    common_average_referenced = np.subtract(
-        ca1_lfp_detection_channels_only, common_average
-    )
-    assert common_average_referenced.shape[0] == len(detection_channels_ca1)
-
-    # Overwrite logic in Jana's code
-    resting_ind = np.repeat(True, common_average_referenced.shape[1])
-
-    candidate_events = get_candidate_ripples(
-        common_average_referenced,
-        detection_channels_ca1,
-        resting_ind,
-        sampling_rate_lfp,
-        DETECTION_METHOD,
-    )
-
-    # Flattening makes further processing easier
-    ripples = [event for events in candidate_events for event in events]
-
-    print(f"Number of candidate ripples: {len(ripples)}")
-
-    ripples = remove_duplicate_ripples(
-        ripples, 0.05, sampling_rate_lfp
-    )  # James 0.3, Buzaki 0.12, elife 0.05
-
-    print(f"Number of ripples after removing duplicates: {len(ripples)}")
-
-    freq_check, CAR_check, SRP_check, CAR_check_lr, SRP_check_lr, ripples = (
-        get_quality_metrics(
-            ripples, common_average_referenced, common_average, sampling_rate_lfp
-        )
-    )
-
-    lfp_state_idx = get_lfp_index_sleep_state(
-        data_folder=data_folder,
-        # This mouse was done with buffering rather than streaming.
-        # Need to remove ripples past the 30 minute mark below
-        n_samples=lfp.shape[1] if mouse != "00053" else 30 * 60 * sampling_rate_lfp,
-        sampling_rate_lfp=sampling_rate_lfp,
-    )
-
-    ripples = [
-        ripple for ripple in ripples if ripple.peak_idx < sampling_rate_lfp * 30 * 60
-    ]
-
-    ripple_state = []
-
-    for ripple in ripples:
-        for state, idxs in lfp_state_idx.items():
-            if ripple.peak_idx in idxs:
-                ripple_state.append(state)
-                break
-
-    assert len(ripple_state) == len(ripples)
-
-    total_passing_all = np.sum(
-        np.array(freq_check) & np.array(CAR_check_lr) & np.array(SRP_check_lr)
-    )
-
-    cache_result = RipplesCache(
-        candidate_events=ripples,
-        common_average_reference_check=CAR_check_lr,
-        frequency_check=freq_check,
-        super_ripple_check=SRP_check_lr,
-        length_recording=(
-            lfp.shape[1] / sampling_rate_lfp if mouse != "00053" else 30 * 60
-        ),
-        state=ripple_state,
-        state_lengths={state: len(list(idxs)) for state, idxs in lfp_state_idx.items()},
-    )
-
-    print(f"Total ripples passing all quality metrics: {total_passing_all}")
-    print(f"Ripple rate {total_passing_all / (lfp.shape[1] / sampling_rate_lfp)}")
-    with open(
-        HERE / "results" / "ripples" / f"{mouse}_{imec}.json",
-        "w",
-    ) as f:
-        json.dump(cache_result.model_dump(), f)
-
-
-def get_lfp_index_sleep_state(
-    data_folder,
-    n_samples: int,
-    sampling_rate_lfp: float,
-    plot: bool = False,
-):
-    """Havent properly tested this yet, but the hacky plot looks fine"""
-    seconds, sleep_state = process_sleep_spreadsheet(data_folder)
-
-    assert abs(seconds[-1] - n_samples / sampling_rate_lfp) < 1
-
-    state_idxs = {
-        "awake": np.array([]),
-        "nrem": np.array([]),
-        "rem": np.array([]),
-        "transition": np.array([]),
-    }
-
-    def map_state(state: str, next_state: str | None) -> str:
-        if state == "nrem" and next_state == "deep nrem":
-            return "nrem"
-        if state == "deep nrem" and next_state == "nrem":
-            return "nrem"
-        if state != next_state:
-            return "transition"
-        if state in {"movement", "awake"}:
-            return "awake"
-        if state in {"deep nrem", "nrem"}:
-            return "nrem"
-        if state == "rem":
-            return "rem"
-        raise ValueError(f"state {state} not recognized")
-
-    for idx, state in enumerate(sleep_state):
-        key = map_state(
-            state, sleep_state[idx + 1] if idx + 1 < len(sleep_state) else None
-        )
-
-        state_idxs[key] = np.append(
-            state_idxs[key],
-            np.arange(idx * sampling_rate_lfp, (idx + 1) * sampling_rate_lfp),
-        )
-
-    included_idxs = np.sort(np.concatenate(list(state_idxs.values())))
-    assert np.all(np.diff(included_idxs) == 1)
-    assert (len(included_idxs) - n_samples) / sampling_rate_lfp < 1
-
-    colors = ["blue", "green", "red"]
-
-    if plot:
-        plt.figure(figsize=(20, 4))
-
-        for idx, state in enumerate(["rem", "awake", "nrem"]):
-            plt.plot(
-                state_idxs[state] / sampling_rate_lfp,
-                np.ones_like(state_idxs[state]),
-                ".",
-                color=colors[idx],
-            )
-            plt.plot(
-                seconds[sleep_state == state],
-                np.ones_like(seconds[sleep_state == state]) + 1,
-                ".",
-                color=colors[idx],
-                label=f"{state}",
-            )
-
-        plt.plot(
-            state_idxs["transition"] / sampling_rate_lfp,
-            np.ones_like(state_idxs["transition"]),
-            ".",
-            color="black",
-        )
-
-        plt.ylim(0, 4)
-        plt.legend()
-
-        1 / 0
-    return state_idxs
+    # detect_ripples(mouse, imec, ca1_low, ca1_high, data_folder, sampling_rate_lfp, lfp)
+    detect_spindles(mouse, imec, rsc_low, rsc_high, data_folder, sampling_rate_lfp, lfp)
 
 
 def get_sync(lfp_path: Path, mouse: str, imec: str) -> np.ndarray:
@@ -409,11 +141,16 @@ def get_ca1_rsc_channels(lfp_file: Path, df: pd.DataFrame) -> tuple[int, int, in
     raise ValueError(f"Could not find channels for {lfp_file.name} in dataframe")
 
 
+# lfp_files = [
+#     Path(
+#         "/Volumes/MarcBusche/Alex/Reactivations/2025-05-20/11150/20250520_g0/20250520_g0_imec0/20250520_g0_t0.imec0.lf.bin"
+#     )
+# ]
+
+
 def main() -> None:
     df = gsheet2df("112rq_5qilRHtYUFnFwpjDQeF4XKyTdY6qJhIwAnykN8", "Sheet1", 1)
-
     umbrella = Path("/Volumes/MarcBusche/Alex/Reactivations/")
-
     lfp_files = list(umbrella.rglob("*.lf.bin"))
     assert len(lfp_files) > 0, "No LFP files found"
 
@@ -430,7 +167,7 @@ def main() -> None:
             assert 30 <= ca1_high - ca1_low <= 70
             assert 80 <= rsc_high - rsc_low <= 120
 
-            get_ripple_rate(
+            get_lfp_signatures(
                 lfp_path=lfp_file.parent,
                 region_channels=(ca1_low, ca1_high, rsc_low, rsc_high),
             )
