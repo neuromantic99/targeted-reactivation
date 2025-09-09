76fcf5600ef8d423ebdd23b1c5aa7c6803309859
neuromantic99 <jrowlandm@gmail.com>
2025-09-09 13:37:38 +0000

    path fixes

--- consts.py
+++ consts.py
@@ -1,16 +1,17 @@
 from pathlib import Path
 
 
-LOCAL_SSD = Path("/Volumes/hard_drive/targeted_reactivation")
-DETECTION_METHOD = "median"  # options 'median' or 'sd'
+LOCAL_SSD = Path("/mnt/local-ssd/james/targeted-reactivation")
+LFP_SYNC_FOLDER = Path("/mnt/local-ssd/james/targeted-reactivation/lfp_syncs")
 
+SERVER_PATH = Path("/mnt/MarcBusche")
+
+KILOSORT_UMBRELLA = SERVER_PATH / "Alex" / "Reactivations"
+
+SERVER_CACHE_PATH = Path(SERVER_PATH / "James/caches/spontaneous_reactivation_strength")
+RIPPLE_PATH = Path(SERVER_PATH / "James/caches/ripples")
+
+
+DETECTION_METHOD = "median"  # options 'median' or 'sd'
 RIPPLE_BAND = [120, 250]
 SUPRA_RIPPLE_BAND = [250, 500]
-
-KILOSORT_UMBRELLA = Path("/Volumes/MarcBusche/Alex/Reactivations")
-SERVER_CACHE_PATH = Path(
-    "/Volumes/MarcBusche/James/caches/spontaneous_reactivation_strength"
-)
-
-RIPPLE_PATH = Path("/Volumes/MarcBusche/James/caches/ripples")
-LFP_SYNC_FOLDER = Path("/Volumes/hard_drive/targeted_reactivation/lfp_syncs")
