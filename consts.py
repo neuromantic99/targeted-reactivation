da08bd8739b5710aa39777200974985f2f63a0c8
neuromantic99 <jrowlandm@gmail.com>
2025-10-03 10:36:55 +0100

    sort of result with all units

--- consts.py
+++ consts.py
@@ -1,17 +1,16 @@
 from pathlib import Path
 
 
-LOCAL_SSD = Path("/mnt/local-ssd/james/targeted-reactivation")
-LFP_SYNC_FOLDER = Path("/mnt/local-ssd/james/targeted-reactivation/lfp_syncs")
+SERVER_PATH = Path("/Volumes/MarcBusche")
+LOCAL_SSD = Path("/Volumes/hard_drive/targeted_reactivation")
+LFP_SYNC_FOLDER = LOCAL_SSD / "lfp_syncs"
 
-SERVER_PATH = Path("/mnt/MarcBusche")
 
 KILOSORT_UMBRELLA = SERVER_PATH / "Alex" / "Reactivations"
-
-SERVER_CACHE_PATH = Path(SERVER_PATH / "James/caches/spontaneous_reactivation_strength")
+SERVER_CACHE_PATH = Path(
+    SERVER_PATH / "James/caches/spontaneous_reactivation_strength_CA1"
+)
 RIPPLE_PATH = Path(SERVER_PATH / "James/caches/ripples")
-
-
 DETECTION_METHOD = "median"  # options 'median' or 'sd'
 RIPPLE_BAND = [120, 250]
 SUPRA_RIPPLE_BAND = [250, 500]
